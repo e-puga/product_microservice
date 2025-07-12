4f0bc56ea1891b27ef56d48d600ef1b9b52d68ce
e-puga <yoel338@hotmail.com>
2025-07-11 22:46:22 -0500

    configuracion actuator

---
+++ src/main/java/com/eypa/msa/product_microservice/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.eypa.msa.product_microservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Data;
+
+@RestController
+@RequestMapping("api/categories")
+@Data
+public class CategoryController {
+
+	@Value("${app.testProp}")
+	public String testProp;
+
+	@GetMapping("test-prop")
+	public String getTestProp() {
+		return this.testProp;
+	}
+
+}
