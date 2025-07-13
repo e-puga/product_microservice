0d6fcd026065d66005718122f152aeb69e4f6a12
epuga <yoel338@hotmail.com>
2025-07-12 22:12:10 -0500

    gupdate getter an setters

--- src/main/java/com/eypa/msa/product_microservice/entity/ProductEntity.java
+++ src/main/java/com/eypa/msa/product_microservice/entity/ProductEntity.java
@@ -17,4 +17,29 @@ public class ProductEntity {
     private String productName;
     private String productDescription;
     private String unitPrice;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public String getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(String unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+    
 }
